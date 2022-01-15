aa2ab2dcb395654deb9e809d9a4dd5bde4a018fa
Julie Evans <juliemevans17@gmail.com>
2022-01-14 21:22:53 -0600

    commit tree d9f3bcb38137dd7997703fbe287de9b93305c37b

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import { cleanup } from "@testing-library/react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1)
+      if(timeRemaining === 0) {
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000)
+    return function cleanup() {
+      clearTimeout(timer)
+    }
+  }, [timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
